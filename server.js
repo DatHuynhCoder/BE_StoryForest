85234d367e8d5f1ef6e6a83509ee4f11f924b8a4
DatHuynhCoder <htdat2004@gmail.com>
2025-03-19 15:43:55 +0700

    set up express middlewares and cors 2

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/connect_DB.js';
 import { Account } from './models/account.model.js';
+import cors from 'cors';
 
 dotenv.config(); // You can access .env vars globally
 
 const app = express();
+
+//add middeleware
+app.use(express.json()); //parse json
+app.use(cors()); //allow cross origin request
+app.use(express.urlencoded({ extended: true })); //allow to handle url encoded data
 
 const PORT = process.env.PORT;
 
