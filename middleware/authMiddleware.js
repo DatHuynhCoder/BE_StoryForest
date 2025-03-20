473d608491dc7947c7d9b04449dac54a236e54ef
le nguyen quynh anh . <huynhanh.170504@gmail.com>
2025-03-20 14:42:56 +0700

    modify middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -5,6 +5,7 @@ export const protect = (req, res, next) => {
 
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         try {
+            // token form: Bearer <token>
             token = req.headers.authorization.split(' ')[1];
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
             req.user = decoded;
