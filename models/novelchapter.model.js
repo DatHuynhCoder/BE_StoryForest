8890a06d3505d344b51740b52f7ff5eefb16990d
le nguyen quynh anh . <huynhanh.170504@gmail.com>
2025-04-01 23:30:19 +0700

    more novel route and modify novel model

---
+++ models/novelchapter.model.js
@@ -0,0 +1,12 @@
+import mongoose, { Schema } from "mongoose";
+
+const NovelChapterSchema = new mongoose.Schema({
+    novelid: { type: String, required: true },
+    chapter_title: { type: String },
+    chapter_link: { type: String },
+    chapter_content: { type: [String], default: [] },
+}, {
+    timestamps: true
+})
+
+export const NovelChapter = mongoose.model('NovelChapter', NovelChapterSchema);
