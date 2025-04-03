127dcbd663ef5ca4bc042eae847ef1fe3cff0686
le nguyen quynh anh . <huynhanh.170504@gmail.com>
2025-04-03 15:11:12 +0700

    fix form of manga data

--- models/book.model.js
+++ models/book.model.js
@@ -1,30 +1,30 @@
 import mongoose, { Schema } from "mongoose";
 
 const BookSchema = new mongoose.Schema({
-  name: {type: String, required: true},
-  bookImg: {
-    url: String,
-    public_id: String
-  },
-  description: {type: String, required: true},
-  type: {type: String, enum: ['sách đọc - audio', 'sách tranh'], default: 'sách tranh'},
-  tag: {type: [String], default: []},
-  pages: {type: Number, required: true},
-  status:{type: String, enum: ['ongoing','completed','hiatus']},
-  numChapter: {type: Number, required: true},
-  vipRequired: {type: Boolean, default: false},
-  authorId: {
-    type: Schema.Types.ObjectId,
-    ref:'Author',
-  },
-  authors: {type: [String], default: []},
-  publishYear: {type: Number, default: 2000},
-  rating: {type: Number, default: 0},
-  numRate: {type: Number, default: 0},
-  views: {type: Number, default: 0},
-  contentRating: {type: String, enum: ['safe','suggestive','erotica']}
-},{
-  timestamps: true
+    name: { type: String, required: true },
+    bookImg: {
+        url: String,
+        public_id: String
+    },
+    description: { type: String, required: true },
+    type: { type: String, enum: ['sách đọc - audio', 'sách tranh'], default: 'sách tranh' },
+    tag: { type: [String], default: [] },
+    pages: { type: Number, required: true },
+    status: { type: String, enum: ['ongoing', 'completed', 'hiatus'] },
+    numChapter: { type: Number, required: true },
+    vipRequired: { type: Boolean, default: false },
+    authorId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Author',
+    },
+    authors: { type: [String], default: [] },
+    publishYear: { type: Number, default: 2000 },
+    rating: { type: Number, default: 0 },
+    numRate: { type: Number, default: 0 },
+    views: { type: Number, default: 0 },
+    contentRating: { type: String, enum: ['safe', 'suggestive', 'erotica'] }
+}, {
+    timestamps: true
 })
 
 export const Book = mongoose.model('Book', BookSchema);
