51acb1eeea552cd87a155051cfc4a083da0dc226
DatHuynhCoder <htdat2004@gmail.com>
2025-05-10 20:56:56 +0700

    get anyone fovorite books

--- routes/user/displaydata.route.js
+++ routes/user/displaydata.route.js
@@ -1,9 +1,12 @@
 import express from 'express'
-import { accountInfo } from '../../controllers/user/displaydata.controller.js';
+import { accountInfo, getAnyFavorite } from '../../controllers/user/displaydata.controller.js';
 
 const displaydataRouter = express.Router();
 
 //display account info (anyone)
 displaydataRouter.get('/account/:id', accountInfo)
+
+//get any favorite account books
+displaydataRouter.get('/favorite/:id', getAnyFavorite);
 
 export default displaydataRouter;
