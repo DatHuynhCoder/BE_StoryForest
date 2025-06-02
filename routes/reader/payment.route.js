fd48f66e6e932e3c652b63f882da4c2a6dace4b0
le nguyen quynh anh . <huynhanh.170504@gmail.com>
2025-06-03 00:19:02 +0700

    add: new payment option

--- routes/reader/payment.route.js
+++ routes/reader/payment.route.js
@@ -4,12 +4,15 @@ import { protect } from '../../middleware/authMiddleware.js';
 
 import {
   createPaymentLink,
+  createPaymentLinkWithOption,
   paymentCallback
 } from '../../controllers/reader/payment.controller.js';
 
 const paymentRouter = express.Router()
 
 paymentRouter.post('/create-payment-link', protect, createPaymentLink)
+
+paymentRouter.post('/create-payment-link-with-option', protect, createPaymentLinkWithOption)
 
 paymentRouter.post('/payment-callback', paymentCallback)
 
