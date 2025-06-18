069f81b89d0122c0600a83086ddd64fcffc79c43
le nguyen quynh anh . <huynhanh.170504@gmail.com>
2025-06-18 08:48:19 +0700

    update: returnUrl query string

--- controllers/reader/payment.controller.js
+++ controllers/reader/payment.controller.js
@@ -1,11 +1,14 @@
 import PayOS from "@payos/node";
+import { xorEncrypt } from "../../utils/XOREncrypt.js";
+
 const payos = new PayOS(
   '596f2353-7de4-42b8-84ae-217713f717be',
   '41b0b93f-1fe2-4b40-81d8-96a22b2fee24',
   '0eed5dc90388324ce053997e49ba6765130e5eff3c661a9f595086847a4d1c17'
 )
 
-const YOUR_DOMAIN = 'https://storyforest.online/payment';
+// const YOUR_DOMAIN = 'https://storyforest.online/payment';
+const YOUR_DOMAIN = 'http://localhost:5173/payment';
 
 export const createPaymentLink = async (req, res) => {
   const userid = req.user.id
@@ -31,7 +34,7 @@ export const createPaymentLink = async (req, res) => {
 
 export const createPaymentLinkWithOption = async (req, res) => {
   const userid = req.user.id
-  const { amount, name } = req.body
+  const { duration, amount, name } = req.body
   const orderCode = Number(`${Date.now()}${Math.floor(10 + Math.random() * 90)}`); // Example: 16832012345671234
   const order = {
     orderCode: orderCode,
@@ -44,7 +47,7 @@ export const createPaymentLinkWithOption = async (req, res) => {
         price: amount,
       },
     ],
-    returnUrl: `${YOUR_DOMAIN}/success?userid=${userid}&name=${name}`,
+    returnUrl: `${YOUR_DOMAIN}/success?userid=${userid}&name=${name}&duration=${duration}$price=${amount}`,
     cancelUrl: `${YOUR_DOMAIN}/cancel`,
   };
   const paymentLink = await payos.createPaymentLink(order);
